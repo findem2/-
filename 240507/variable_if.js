352a502a2adaf39be45354db386f2cf911012bf8
findem2 <thsalsqhr@gmail.com>
2024-05-07 16:25:05 +0900

    feat: refeat reset

---
+++ 240507/variable_if.js
@@ -0,0 +1,23 @@
+let isright = true;
+
+// isright가 true면 false로 할당
+if(isright == true) { 
+  isright = false; 
+}
+
+// 프로퍼티 a는 1 b는 손민복을 가지고 있는 객체를 obj로 초기화 한다
+// 상수obj를 프로퍼티 a는 1 프로퍼티 b는 손민복을 가지고 있는 객체로 초기화 한다.
+const obj = { a: 1, b: "손민복" };
+
+// 숫자 1,2,3을 가지고 있는 배열을 변수 arr에 초기화
+let arr = [1, 2, 3];
+
+!3 // false
+
+!"" // true
+
+!undefined // true
+
+!!!0 // true
+
+'' || 0 || undefined || null // 
