1576dbcfdec8c7e734d410735160d11e2880277a
findem2 <thsalsqhr@gmail.com>
2024-05-09 01:31:56 +0900

    feat: add 24.05.09 01:20

---
+++ 240508/check.js
@@ -0,0 +1,31 @@
+let num = 10;
+
+// 만약에 num이 20보다 작으면 2를 곱하자
+if (num < 20 ){}
+
+let student = {
+name : "손민복",
+age : 29
+};
+
+!`` // true
+
+3 ** 3
+
+for (let i = 0; i < 10; i++ ){
+    console.log(i + 1)
+};
+
+
+// 만약에 job이 학생이고 age가 29면 손민복을 출력해라
+if (job == "학생" && age == 29) {
+    console.log("손민복");
+}
+
+// 만약에 job이 대학생이고 status가 휴학중이거나 job이 취업준비생이고 status가 대학졸업이면 수강신청 가능을 출력해라.
+if (
+  (job == "대학생" && status == "휴학중") ||
+  (job == "취업준비생" && status == "대학졸업")
+  ) {
+    console.log("수강신청 가능");
+}
---
+++ temp/240508.cpp
@@ -0,0 +1,6 @@
+// C++ 언어
+int main(char[] args) {
+  char name = 'a';
+  char[] name2 = "sadf";
+  return 0;
+}
